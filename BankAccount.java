9223324be31c50b5b4b8951b9195b352dcd9b7f2
PedroBarge <pedro525tavares@gmail.com>
2024-01-02 12:22:17 +0000

    done

---
+++ BankAccount.java
@@ -0,0 +1,28 @@
+public class BankAccount {
+    private double balance;
+
+    public BankAccount() {
+        balance = 0.0;
+    }
+
+    public void deposit(double amount) {
+        synchronized (this) {
+            balance += amount;
+            System.out.println("Deposit: " + amount);
+            System.out.println("Balance after deposit: " + balance);
+        }
+    }
+
+    public void withdraw(double amount) {
+        synchronized (this) {
+            if (balance >= amount) {
+                balance -= amount;
+                System.out.println("Withdrawal: " + amount);
+                System.out.println("Balance after withdrawal: " + balance);
+            } else {
+                System.out.println("Try to Withdraw: " + amount);
+                System.out.println("Insufficient funds. Withdrawal cancelled.");
+            }
+        }
+    }
+}
---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        BankAccount account = new BankAccount();
+
+        Thread depositThread1 = new Thread(() -> account.deposit(1000));
+        Thread depositThread2 = new Thread(() -> account.deposit(300));
+        Thread withdrawalThread1 = new Thread(() -> account.withdraw(150));
+        Thread withdrawalThread2 = new Thread(() -> account.withdraw(1200));
+
+        depositThread1.start();
+        depositThread1.join();
+        depositThread2.start();
+        depositThread1.join();
+        withdrawalThread1.start();
+        withdrawalThread1.join();
+        withdrawalThread2.start();
+        withdrawalThread2.join();
+
+    }
+}
